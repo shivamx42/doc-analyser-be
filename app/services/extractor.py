ebf3f9ce2897fae65eff055640e4a57b8bceb2cc
Shivam <shivamjhax@gmail.com>
2026-04-19 18:19:16 +0530

    Added OCR functionality to PDF extraction

--- app/services/extractor.py
+++ app/services/extractor.py
@@ -1,5 +1,8 @@
 import fitz
 from dataclasses import dataclass
+from PIL import Image
+import io
+import pytesseract
 
 
 @dataclass
@@ -17,6 +20,19 @@ def extract_from_pdf(content: bytes) -> ExtractionResult:
         text = page.get_text()
         if(text.strip()):  # Only add pages that have text(that is skipping blank pages and pages with only images)
             pages.append(text)
+    
+    if len(pages) == 0:
+        for page_num in range(len(doc)):
+            page = doc[page_num]
+
+            # PDF page to image
+            pimg = page.get_pixmap(matrix=fitz.Matrix(2, 2))  # 2x zoom
+            image_data = pimg.tobytes("png")
+            image = Image.open(io.BytesIO(image_data))
+            
+            text = pytesseract.image_to_string(image)
+            if text.strip():
+                pages.append(text)
 
     doc.close()
 
