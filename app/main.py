4ea1cf457afa7a521dfc78da4a2021a02197b2c7
Shivam <shivamjhax@gmail.com>
2026-05-08 22:32:14 +0530

    Refactored CORS configuration to use environment variable

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from app.routers import upload, query, auth, deleteDocument
 from fastapi.middleware.cors import CORSMiddleware
@@ -5,9 +7,11 @@ from app.routers import getDocuments
 
 app = FastAPI()
 
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=CORS_ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
